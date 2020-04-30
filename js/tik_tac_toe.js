7658dae89655bec3e9ab4f319e2bc5ffdf95a7a0
aaqibgouher <aaqibgouher@gmail.com>
2020-04-30 13:16:08 +0530

    added new files

--- js/tik_tac_toe.js
+++ js/tik_tac_toe.js
@@ -1,10 +1,29 @@
 class TikTacToe{
     constructor(){
+        this.arr = [];
+        this.arr.push(["-","-","-"]);
+        this.arr.push(["-","-","-"]);
+        this.arr.push(["-","-","-"]);
+        this.row = null;
+        this.column = null;
+    }
 
+    print(){
+         for(var i in this.arr){
+            console.log(`${this.arr[i][0]} ${this.arr[i][1]} ${this.arr[i][2]}`)
+        }
+    }
+
+    insert(){
+        this.arr[this.row-1][this.column-1] = "x";
+        this.print();
     }
 
     start(){
-
+        console.log("Game Started ..");
+        // for(var i in this.arr){
+        //     console.log(`${this.arr[i][0]} ${this.arr[i][1]} ${this.arr[i][2]}`)
+        // }
     }
 
     restart(){
@@ -14,11 +33,18 @@ class TikTacToe{
     // x and y are tik tac toe cordinates 1-index
     // there is a 3x3 matrix (or array 2d) but i will pas x an y as index 1
     turn(x, y){
-
+        if(x <= 3 && y <= 3){
+            this.row = x;
+            this.column = y;
+            this.insert();
+        }
+        else{
+            console.log("Index is Invalid..");
+        }
     }
 }
 
-var t = TikTacToe();
+var t = new TikTacToe();
 
 t.start();
 // game started
@@ -32,31 +58,31 @@ t.turn(2,2);
 // - - -
 
 t.turn(1,1);
-// O - -
-// - X -
-// - - -
+// // O - -
+// // - X -
+// // - - -
 
-t.turn(1,1);
-// already filled
+// t.turn(1,1);
+// // already filled
 
-t.turn(3,1);
-// O - -
-// - X -
-// X - -
+// t.turn(3,1);
+// // O - -
+// // - X -
+// // X - -
 
-t.turn(1,2);
-// O O -
-// - X -
-// X - -
+// t.turn(1,2);
+// // O O -
+// // - X -
+// // X - -
 
-t.turn(1,3);
-// O O X
-// - X -
-// X - -
-// player X won
+// t.turn(1,3);
+// // O O X
+// // - X -
+// // X - -
+// // player X won
 
-t.turn(1,3);
-// game over please restart
+// t.turn(1,3);
+// // game over please restart
 
-t.restart();
-// game started
+// t.restart();
+// // game started
